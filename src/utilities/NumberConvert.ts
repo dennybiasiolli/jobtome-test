ea7249c7b781bcbab109ace3943e8e68a56ecd06
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2019-06-05 16:22:33 +0200

    utilities(NumberConvert): adding check for INFINITY numbers

--- src/utilities/NumberConvert.ts
+++ src/utilities/NumberConvert.ts
@@ -1,3 +1,54 @@
-export function numberToEnglish(strNumber: string): string {
-    return 'Unimplemented';
+const baseNumbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+];
+
+const decNumbers = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+];
+
+
+export function numberToEnglish(originalNumber: number | string): string {
+    let num: number;
+    if (typeof originalNumber === 'string') {
+        num = parseFloat(originalNumber);
+    } else {
+        num = originalNumber;
+    }
+    if (isNaN(num)) {
+        throw new Error(`"${originalNumber}" is not a number.`);
+    }
+    if (num === Number.POSITIVE_INFINITY) {
+        return 'infinity';
+    } else if (num === Number.NEGATIVE_INFINITY) {
+        return 'negative infinity';
+    }
+    return 'unimplemented';
 };
